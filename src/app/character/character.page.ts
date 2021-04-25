c342d0b945dd7bc8bd76d785fd8e65a119f5b269
abixai25 <73407737+abixai25@users.noreply.github.com>
2021-04-24 21:08:21 -0400

    create app

---
+++ src/app/character/character.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: 'app-character',
+  templateUrl: './character.page.html',
+  styleUrls: ['./character.page.scss'],
+})
+export class CharacterPage implements OnInit {
+
+  characters = [];
+
+  constructor(private http: HttpClient) {}
+
+  async ngOnInit() {
+    this.http
+      .get<any>("https://rickandmortyapi.com/api/character")
+      .subscribe((res) => {
+        console.log(res)
+        this.characters = res.results;
+      });
+  }
+}
